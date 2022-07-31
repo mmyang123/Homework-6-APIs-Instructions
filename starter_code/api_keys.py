3f6ae67d40869e476e774d5a184ee8de8bc004d1
FirstName LastName <maimyang@live.com>
2022-07-31 17:29:28 -0500

    Added WeatherPy and VacationPy files

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a065e63965c6f652be11d15d04aa7438"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB7b59i3vUmY-iiiu2mUZUQEdKcUvuIvjQ"
